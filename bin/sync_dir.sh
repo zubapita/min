29a19beb32d544556239a88ca58f94af6763e90a
zubapita <tomoyun@zubapita.jp>
2015-10-11 04:10:55 +0900

    fix sync_dir.sh

--- bin/sync_dir.sh
+++ bin/sync_dir.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 #
-# Sync to deploy server
+# Deploy
+# Sync to production run server
 #
 RSYNC_TEST="-auvzn -e ssh -O"
 RSYNC_EXEC="-auvz -e ssh -O"
 EXCLUDE="--exclude=.svn --exclude=.git --exclude=.editorconfig --exclude=.gitignore --exclude=var/ --exclude=tmp/"
-APP_NAME=booklover
+DEPLOY_ROOT="account@domain:~/Web/{$appName}"
 
-rsync ${RSYNC_TEST} ${EXCLUDE} ../* edit@dev01.minutesbook.jp:~/bin/report/app/
+rsync ${RSYNC_TEST} ${EXCLUDE} ../* ${DEPLOY_ROOT}/
 echo ""
 while : ; do
 	echo -n "Are you sure ? [y/n] : "
@@ -15,7 +16,7 @@ while : ; do
 	if [ "$answer" = "y" -o "$answer" = "Y" ] ; then
 		echo "sync"
 		echo "";
-		rsync ${RSYNC_EXEC} ${EXCLUDE} ../* edit@dev01.minutesbook.jp:~/bin/report/app/
+		rsync ${RSYNC_EXEC} ${EXCLUDE} ../* ${DEPLOY_ROOT}/
 
 		exit
 	elif [ "$answer" = "n" -o "$answer" = "N" ] ; then
