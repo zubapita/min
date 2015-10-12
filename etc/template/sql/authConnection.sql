404d3f15bf542a588abe9e0cfd86bdc0096a339f
zubapita <tomoyun@zubapita.jp>
2015-10-12 14:29:54 +0900

    change column hybridauthSession to text.

--- etc/template/sql/authConnection.sql
+++ etc/template/sql/authConnection.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS authConnection;
 CREATE TABLE `authConnection` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `userId` int(11) NOT NULL,
-  `provider` varchar(20) NOT NULL DEFAULT '',
-  `hybridauthSession` varchar(255) NOT NULL DEFAULT '',
+  `provider` varchar(20) NOT NULL,
+  `hybridauthSession` text NOT NULL,
   `updateAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
