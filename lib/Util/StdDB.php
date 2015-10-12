cdf3695d46435000b2e083cd2d47202ee9e6ac26
zubapita <tomoyun@zubapita.jp>
2015-10-12 19:50:46 +0900

    add StdDb.php.

---
+++ lib/Util/StdDB.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * データベース操作クラス
+ * 
+ * DBAccessクラスをそのままインスタンス化して使うためのクラス
+ * テーブルクラスを使わずに、create tableなどのデータベース操作を行うときに使用する
+ * 
+ * ex) データベース mydb のデータベースクラスファイルが作成済みとする
+ * 
+ * $DBSpec = new mydb;
+ * $MyDB = new StdDB($DBSpec);
+ * $sql = "create table mytable (id int(11) AUTO_INCREMENT, name varchar(10)";
+ * $result = $MyDB->query($sql);
+ * 
+ */
+// 
+class StdDB extends DBAccess {} 
