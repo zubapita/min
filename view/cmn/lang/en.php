43feff3caa5b16ecfecc25863ab2321ab8e9d756
zubapita <tomoyun@zubapita.jp>
2015-11-03 10:48:05 +0900

    update header for sample menu.

--- view/cmn/lang/en.php
+++ view/cmn/lang/en.php
@@ -1,4 +1,19 @@
 <?php
 return [
+    'add' => 'Add New Item',
+    'edit' => 'Edit Item',
+    'list' => 'Item List',
+    'spots' => 'Spots',
+    'books' => 'Books',
+    'others' => 'Others',
+    'uploadImg' => 'Upload Images',
+    'gmap' => 'Google Maps',
+
     'welcome' => 'Welcome ',
+    'login' => 'Login',
+    'logout' => 'Logout',
+    'guest' => 'Guest User',
+    'jp-san' => '',
+    'loginWithRegisterdAccount' => 'Login with Regsiterd Account',
+    
 ];
--- view/cmn/lang/jp.php
+++ view/cmn/lang/jp.php
@@ -1,5 +1,20 @@
 <?php
 return [
+    'add' => '新規登録',
+    'edit' => '編集',
+    'list' => '一覧',
+    'spots' => 'スポット',
+    'books' => '書籍',
+    'others' => 'その他',
+    'uploadImg' => '画像アップロード',
+    'gmap' => 'Googleマップ',
+
     'welcome' => 'ようこそ、',
+    'login' => 'ログイン',
+    'logout' => 'ログアウト',
+    'guest' => 'ゲスト',
+    'jp-san' => 'さん',
+    'loginWithRegisterdAccount' => '登録アカウントでログイン',
+    
 ];
 
